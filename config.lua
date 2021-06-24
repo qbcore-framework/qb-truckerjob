b6c1da1b89524aad0ad6c3168e7df3192f25a238
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-06-24 13:53:07 +0200

    vector values

--- config.lua
+++ config.lua
@@ -5,96 +5,96 @@ Config.BailPrice = 250
 Config.Locations = {
     ["main"] = {
         label = "Truck Shed",
-        coords = {x = 153.68, y = -3211.88, z = 5.91, h = 274.5},
+        coords = vector4(153.68, -3211.88, 5.91, 274.5),
     },
     ["vehicle"] = {
         label = "Truck Storage",
-        coords = {x = 141.12, y = -3204.31, z = 5.85, h = 267.5},
+        coords = vector4(141.12, -3204.31, 5.85, 267.5),
     },
     ["stores"] ={
         [1] = {
             name = "ltdgasoline",
-            coords = {x = -41.07, y = -1747.91, z = 29.4, h = 137.5},
+            coords = vector4(-41.07, -1747.91, 29.4, 137.5),
         },
         [2] = {
             name = "247supermarket",
-            coords = {x = 31.62, y = -1315.87, z = 29.52, h = 179.5},
+            coords = vector4(31.62, -1315.87, 29.52, 179.5),
         },
         [3] = {
             name = "robsliquor",
-            coords = {x = -1226.48, y = -907.58, z = 12.32, h = 119.5},
+            coords = vector4(-1226.48, -907.58, 12.32, 119.5),
         },
         [4] = {
             name = "ltdgasoline2",
-            coords = {x = -714.13, y = -909.13, z = 19.21, h = 0.5},
+            coords = vector4(-714.13, -909.13, 19.21, 0.5),
         },
         [5] = {
             name = "robsliquor2",
-            coords = {x = -1469.78, y = -366.72, z = 40.2, h = 138.5},
+            coords = vector4(-1469.78, -366.72, 40.2, 138.5),
         },
         [6] = {
             name = "ltdgasoline3",
-            coords = {x = -1829.15, y = 791.99, z = 138.26, h = 46.5},
+            coords = vector4(-1829.15, 791.99, 138.26, 46.5),
         },
         [7] = {
             name = "robsliquor3",
-            coords = {x = -2959.92, y = 396.77, z = 15.02, h = 178.5},
+            coords = vector4(-2959.92, 396.77, 15.02, 178.5),
         },
         [8] = {
             name = "247supermarket2",
-            coords = {x = -3047.58, y = 589.89, z = 7.78, h = 199.5},
+            coords = vector4(-3047.58, 589.89, 7.78, 199.5),
         },
         [9] = {
             name = "247supermarket3",
-            coords = {x = -3245.85, y = 1008.25, z = 12.83, h = 90.5},
+            coords = vector4(-3245.85, 1008.25, 12.83, 90.5),
         },
         [8] = {
             name = "247supermarket4",
-            coords = {x = 1735.54, y = 6416.28, z = 35.03, h = 332.5},
+            coords = vector4(1735.54, 6416.28, 35.03, 332.5),
         },
         [9] = {
             name = "247supermarket5",
-            coords = {x = 1702.84, y = 4917.28, z = 42.22, h = 323.5},
+            coords = vector4(1702.84, 4917.28, 42.22, 323.5),
         },
         [10] = {
             name = "247supermarket6",
-            coords = {x = 1960.47, y = 3753.59, z = 32.26, h = 127.5},
+            coords = vector4(1960.47, 3753.59, 32.26, 127.5),
         },
         [11] = {
             name = "robsliquor4",
-            coords = {x = 1169.27, y = 2707.7, z = 38.15, h = 267.5},
+            coords = vector4(1169.27, 2707.7, 38.15, 267.5),
         },
         [12] = {
             name = "247supermarket7",
-            coords = {x = 543.47, y = 2658.81, z = 42.17, h = 277.5},
+            coords = vector4(543.47, 2658.81, 42.17, 277.5),
         },
         [13] = {
             name = "247supermarket8",
-            coords = {x = 2678.09, y = 3288.43, z = 55.24, h = 61.5},
+            coords = vector4(2678.09, 3288.43, 55.24, 61.5),
         },
         [14] = {
             name = "247supermarket9",
-            coords = {x = 2553.0, y = 399.32, z = 108.61, h = 179.5, r = 1.0},
+            coords = vector4(2553.0, 399.32, 108.61, 179.5),
         },
         [15] = {
             name = "ltdgasoline4",
-            coords = {x = 1155.97, y = -319.76, z = 69.2, h = 17.5},
+            coords = vector4(1155.97, -319.76, 69.2, 17.5),
         },
         [16] = {
             name = "robsliquor5",
-            coords = {x = 1119.78, y = -983.99, z = 46.29, h = 287.5},
+            coords = vector4(1119.78, -983.99, 46.29, 287.5),
         },
         [17] = {
             name = "247supermarket9",
-            coords = {x = 382.13, y = 326.2, z = 103.56, h = 253.5},
+            coords = vector4(382.13, 326.2, 103.56, 253.5),
         },
         [18] = {
             name = "hardware",
-            coords = {x = 89.33, y = -1745.44, z = 30.08, h = 143.5},
+            coords = vector4(89.33, -1745.44, 30.08, 143.5),
         },
         [19] = {
             name = "hardware2",
-            coords = {x = 2704.09, y = 3457.55, z = 55.53, h = 339.5},
+            coords = vector4(2704.09, 3457.55, 55.53, 339.5),
         },
     },
 }
