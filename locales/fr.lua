787148c2ecb026227db8c64f4e39d752321e4b0c
Tony <97451137+TonybynMp4@users.noreply.github.com>
2022-03-29 21:40:51 +0200

    Apply suggestions
    Co-authored-by: Restray <contact@restray.org>

--- locales/fr.lua
+++ locales/fr.lua
@@ -2,7 +2,7 @@ local Translations = {
     error = {
         no_deposit = "$%{value} dépôt requis",
         cancelled = "Annulé",
-        vehicle_not_correct = "Ce n'est pas un véhicule Commercial!",
+        vehicle_not_correct = "Ce n'est pas un véhicule commercial!",
         no_driver = "Vous devez être le conducteur du véhicule!",
         no_work_done = "Vous n'avez pas encore travaillé.",
     },
@@ -11,11 +11,11 @@ local Translations = {
         paid_with_bank = "$%{value} Depôt payé avec la banque",
         refund_to_cash = "$%{value} Depôt payé avec l'argent",
         you_earned = "Vous avez gagné $%{value}",
-        payslip_time = "Vous avez finis de travaillé... temps de récuperer votre paie",
+        payslip_time = "Vous avez fini de travailler... Temps de récupérer votre paie",
     },
     menu = {
         header = "Camions disponibles",
-        close_menu = "⬅ Fermer le Menu",
+        close_menu = "⬅ Fermer le menu",
     },
     mission = {
         store_reached = "Vous avez atteint la destination, Récuperez une boite dans le coffre avec [E] et livrez-la au marqueur.",
