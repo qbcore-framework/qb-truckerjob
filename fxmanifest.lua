ab192e3d73ab9da6e04de236fce9d6c085d9c5d2
buddizer <buddizer@mbp.local>
2022-05-14 18:31:58 -0600

    fix config.lua formatting

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,15 +5,15 @@ description 'QB-TruckerJob'
 version '1.0.0'
 
 shared_scripts {
-	'@qb-core/shared/locale.lua',
-	'config.lua',
-	'locales/en.lua',
+    '@qb-core/shared/locale.lua',
+    'config.lua', -- Change client/server language
+    'locales/en.lua',
 }
 
 client_scripts {
-	'@PolyZone/client.lua',
-	'@PolyZone/BoxZone.lua',
-	'@PolyZone/ComboZone.lua',
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/ComboZone.lua',
     'client/main.lua'
 }
 
