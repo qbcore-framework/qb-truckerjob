0b06868e895586ab8a11c513a81b078f5d876958
buddizer <buddizer@mbp.local>
2022-05-14 18:30:24 -0600

    add es.lua and fix some en.lua phrases

--- locales/en.lua
+++ locales/en.lua
@@ -3,13 +3,13 @@ local Translations = {
         no_deposit = "$%{value} Deposit Required",
         cancelled = "Cancelled",
         vehicle_not_correct = "This is not a commercial vehicle!",
-        no_driver = "You must be the driver to do this..",
-        no_work_done = "You haven't done any work yet..",
+        no_driver = "You must be the driver to do this",
+        no_work_done = "You haven't done any work yet",
     },
     success = {
         paid_with_cash = "$%{value} Deposit Paid With Cash",
         paid_with_bank = "$%{value} Deposit Paid From Bank",
-        refund_to_cash = "$%{value} Deposit Paid With Cash",
+        refund_to_cash = "$%{value} Deposit Refunded With Cash",
         you_earned = "You Earned $%{value}",
         payslip_time = "You Went To All The Shops .. Time For Your Payslip!",
     },
@@ -19,13 +19,13 @@ local Translations = {
     },
     mission = {
         store_reached = "Store reached, get a box in the trunk with [E] and deliver to marker",
-        take_box = "Take A Box Of Products",
-        deliver_box = "Deliver Box Of Products",
+        take_box = "Taking A Box Of Products",
+        deliver_box = "Delivering Box Of Products",
         another_box = "Get another Box Of Products",
         goto_next_point = "You Have Delivered All Products, To The Next Point",
     },
     info = {
-        deliver_e = "~g~E~w~ - Deliver Products",
+        deliver_e = "[E] - Deliver Products",
         deliver = "Deliver Products",
     }
 }
