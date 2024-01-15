df4298cfd54908717f1d712f4e5c84feb428a3b4
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2024-01-15 00:34:32 -0600

    Money reasons

--- server/main.lua
+++ server/main.lua
@@ -7,22 +7,22 @@ RegisterNetEvent('qb-trucker:server:DoBail', function(bool, vehInfo)
     if bool then
         if Player.PlayerData.money.cash >= Config.TruckerJobTruckDeposit then
             Bail[Player.PlayerData.citizenid] = Config.TruckerJobTruckDeposit
-            Player.Functions.RemoveMoney('cash', Config.TruckerJobTruckDeposit, "tow-received-bail")
-            TriggerClientEvent('QBCore:Notify', src, Lang:t("success.paid_with_cash", {value = Config.TruckerJobTruckDeposit}), 'success')
+            Player.Functions.RemoveMoney('cash', Config.TruckerJobTruckDeposit, 'tow-received-bail')
+            TriggerClientEvent('QBCore:Notify', src, Lang:t('success.paid_with_cash', { value = Config.TruckerJobTruckDeposit }), 'success')
             TriggerClientEvent('qb-trucker:client:SpawnVehicle', src, vehInfo)
         elseif Player.PlayerData.money.bank >= Config.TruckerJobTruckDeposit then
             Bail[Player.PlayerData.citizenid] = Config.TruckerJobTruckDeposit
-            Player.Functions.RemoveMoney('bank', Config.TruckerJobTruckDeposit, "tow-received-bail")
-            TriggerClientEvent('QBCore:Notify', src, Lang:t("success.paid_with_bank", {value = Config.TruckerJobTruckDeposit}), 'success')
+            Player.Functions.RemoveMoney('bank', Config.TruckerJobTruckDeposit, 'tow-received-bail')
+            TriggerClientEvent('QBCore:Notify', src, Lang:t('success.paid_with_bank', { value = Config.TruckerJobTruckDeposit }), 'success')
             TriggerClientEvent('qb-trucker:client:SpawnVehicle', src, vehInfo)
         else
-            TriggerClientEvent('QBCore:Notify', src, Lang:t("error.no_deposit", {value = Config.TruckerJobTruckDeposit}), 'error')
+            TriggerClientEvent('QBCore:Notify', src, Lang:t('error.no_deposit', { value = Config.TruckerJobTruckDeposit }), 'error')
         end
     else
         if Bail[Player.PlayerData.citizenid] then
-            Player.Functions.AddMoney('cash', Bail[Player.PlayerData.citizenid], "trucker-bail-paid")
+            Player.Functions.AddMoney('cash', Bail[Player.PlayerData.citizenid], 'trucker-bail-paid')
             Bail[Player.PlayerData.citizenid] = nil
-            TriggerClientEvent('QBCore:Notify', src, Lang:t("success.refund_to_cash", {value = Config.TruckerJobTruckDeposit}), 'success')
+            TriggerClientEvent('QBCore:Notify', src, Lang:t('success.refund_to_cash', { value = Config.TruckerJobTruckDeposit }), 'success')
         end
     end
 end)
@@ -40,14 +40,14 @@ RegisterNetEvent('qb-trucker:server:01101110', function(drops)
     local payment = (Config.TruckerJobDropPrice * drops + bonus)
     payment = payment - (math.ceil(payment / 100) * Config.TruckerJobPaymentTax)
     Player.Functions.AddJobReputation(drops)
-    Player.Functions.AddMoney("bank", payment, "trucker-salary")
-    TriggerClientEvent('QBCore:Notify', src, Lang:t("success.you_earned", {value = payment}), 'success')
+    Player.Functions.AddMoney('bank', payment, 'trucker-salary')
+    TriggerClientEvent('QBCore:Notify', src, Lang:t('success.you_earned', { value = payment }), 'success')
 end)
 
 RegisterNetEvent('qb-trucker:server:nano', function()
-    local chance = math.random(1,100)
+    local chance = math.random(1, 100)
     if chance > 26 then return end
     local xPlayer = QBCore.Functions.GetPlayer(tonumber(source))
-    xPlayer.Functions.AddItem("cryptostick", 1, false)
-    TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items["cryptostick"], "add")
+    xPlayer.Functions.AddItem('cryptostick', 1, false)
+    TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['cryptostick'], 'add')
 end)
