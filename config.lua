50856bd384f1215329d15209e9edfa4b78320764
t0retto <tonyfarlacmlv@gmail.com>
2024-05-18 22:07:38 +0100

    style: standardize string quotes from " to '

--- config.lua
+++ config.lua
@@ -10,58 +10,58 @@ Config.TruckerJobBonus = 20 -- this is a percentage to calculate bonus over 5 de
 Config.TruckerJobPaymentTax = 15
 
 Config.TruckerJobLocations = {
-    ["main"] = {
-        label = "Truck Shed",
+    ['main'] = {
+        label = 'Truck Shed',
         coords = vector4(153.68, -3211.88, 5.91, 274.5),
     },
-    ["vehicle"] = {
-        label = "Truck Storage",
+    ['vehicle'] = {
+        label = 'Truck Storage',
         coords = vector4(141.12, -3204.31, 5.85, 267.5),
     },
 }
 
 Config.TruckerJobVehicles = {
-    ["rumpo"] = {
-        ["label"] = "Rumpo Delivery Van",
-        ["cargodoors"] = {
+    ['rumpo'] = {
+        ['label'] = 'Rumpo Delivery Van',
+        ['cargodoors'] = {
             [0] = 2,
             [1] = 3
         },
-        ["trunkpos"] = 1.5
+        ['trunkpos'] = 1.5
     },
-    ["benson"] = {
-        ["label"] = "Benson Box Truck",
-        ["jobrep"] = 0,
-        ["cargodoors"] = {
+    ['benson'] = {
+        ['label'] = 'Benson Box Truck',
+        ['jobrep'] = 0,
+        ['cargodoors'] = {
             [0] = 5
         },
-        ["trunkpos"] = 3
+        ['trunkpos'] = 3
     },
-    ["mule5"] = {
-        ["label"] = "Mule Box Truck",
-        ["jobrep"] = 0,
-        ["cargodoors"] = {
+    ['mule5'] = {
+        ['label'] = 'Mule Box Truck',
+        ['jobrep'] = 0,
+        ['cargodoors'] = {
             [0] = 2,
             [1] = 3
         },
-        ["trunkpos"] = 1.5
+        ['trunkpos'] = 1.5
     },
-    ["pounder"] = {
-        ["label"] = "Pounder Box Truck",
-        ["jobrep"] = 0,
-        ["cargodoors"] = {
+    ['pounder'] = {
+        ['label'] = 'Pounder Box Truck',
+        ['jobrep'] = 0,
+        ['cargodoors'] = {
             [0] = 2,
             [1] = 3
         },
-        ["trunkpos"] = 7
+        ['trunkpos'] = 7
     },
-    ["boxville4"] = {
-        ["label"] = "Boxville StepVan",
-        ["jobrep"] = 0,
-        ["cargodoors"] = {
+    ['boxville4'] = {
+        ['label'] = 'Boxville StepVan',
+        ['jobrep'] = 0,
+        ['cargodoors'] = {
             [0] = 2,
             [1] = 3
         },
-        ["trunkpos"] = 1.5
+        ['trunkpos'] = 1.5
     },
 }
